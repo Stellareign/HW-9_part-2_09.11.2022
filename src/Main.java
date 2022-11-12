b61708e0e45d701e81f9e0bc843c15531be28340
Stellareign <tuguran@mail.ru>
2022-11-12 20:40:51 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,14 @@
-
-import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашка 9, МАССИВЫ, часть 2, от 09.11.2022 - Задача 3"); // о, ну это было легко!
-        System.out.println("Траты по дням за месяц составили " + Arrays.toString(arr));
-        int summExpensesMonth = 0;
-        int middleExpensesMonth = 0;
-        for (int i = 0; i < arr.length; i++) {
-            summExpensesMonth += arr[i];
-            middleExpensesMonth = summExpensesMonth / arr.length;
-        }
-        System.out.println("Сумма трат за месяц составила " + summExpensesMonth + " рублей");
-        System.out.println("Средняя сумма трат за месяц составила " + middleExpensesMonth + " рублей");
-    }
-    static int[] arr = generateRandomArray();
+        System.out.println("Домашка 9, МАССИВЫ, часть 2, от 09.11.2022 - Задача 4"); // о, ну это было легко!
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char fullName = 0;
+        for (int i = reverseFullName.length-1; i >= 0; i--) {
+        fullName = reverseFullName[i];
+            System.out.print(fullName);
 
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
         }
-        return arr;
+
     }
 }
 
