b6a6455ac799a9b40a51a2a08cb967033cf183f1
Stellareign <tuguran@mail.ru>
2022-11-12 19:54:34 +0300

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,16 @@
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашка 9, МАССИВЫ, часть 2, от 09.11.2022 - Задача 2"); // найти max и min. Просто оловой об стену.
+        System.out.println("Домашка 9, МАССИВЫ, часть 2, от 09.11.2022 - Задача 3"); // о, ну это было легко!
         System.out.println("Траты по дням за месяц составили " + Arrays.toString(arr));
-        int maxExpensesMonth = arr[0];
-        int minExpensesMonth = arr[0];
+        int summExpensesMonth = 0;
+        int middleExpensesMonth = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < minExpensesMonth)
-                minExpensesMonth = arr[i];
-            if (arr[i] > maxExpensesMonth)
-                maxExpensesMonth = arr[i];
+            summExpensesMonth += arr[i];
+            middleExpensesMonth = summExpensesMonth / arr.length;
         }
-        System.out.println("Минимальная сумма трат за месяц составила " + minExpensesMonth + " рублей");
-        System.out.println("Максимальная сумма трат за месяц составила " + maxExpensesMonth + " рублей");
+        System.out.println("Сумма трат за месяц составила " + summExpensesMonth + " рублей");
+        System.out.println("Средняя сумма трат за месяц составила " + middleExpensesMonth + " рублей");
     }
     static int[] arr = generateRandomArray();
 
